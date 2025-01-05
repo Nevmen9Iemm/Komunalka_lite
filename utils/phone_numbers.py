2f945f9bc2abe1f5d772d80c58c5ddc5a307f0c8
Andrey.S <161717784+Nevmen9Iemm@users.noreply.github.com>
2025-01-05 22:18:58 +0200

    added phone number

--- utils/phone_numbers.py
+++ utils/phone_numbers.py
@@ -4,7 +4,7 @@ from tests.test_input_numbers import validate_phone_number
 
 def phone_numbers():
     # Запит номера телефону
-    phone_number = input("Введіть номер телефону у форматі 380XXXXXXXXXX: ")
+    phone_number = '380938094594' #input("Введіть номер телефону у форматі 380XXXXXXXXXX: ")
     if not validate_phone_number(phone_number):
         print("Неправильний формат номера телефону. Будь ласка, спробуйте знову.")
         main()
