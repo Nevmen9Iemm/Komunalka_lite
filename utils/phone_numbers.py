0643d857beadc6bd859634e42c2c7922142f9530
Nevmen9Iemm <andreysumnevich@gmail.com>
2025-01-13 11:50:18 +0200

    edit if not validate phone number

--- utils/phone_numbers.py
+++ utils/phone_numbers.py
@@ -4,9 +4,9 @@ from tests.test_input_numbers import validate_phone_number
 
 def phone_numbers():
     # Запит номера телефону
-    phone_number = '380938094594' #input("Введіть номер телефону у форматі 380XXXXXXXXXX: ")
+    phone_number = input("Введіть номер телефону у форматі 380XXXXXXXXXX: ")
     if not validate_phone_number(phone_number):
         print("Неправильний формат номера телефону. Будь ласка, спробуйте знову.")
-        main()
+        phone_numbers()
 
     choose_services(phone_number)
